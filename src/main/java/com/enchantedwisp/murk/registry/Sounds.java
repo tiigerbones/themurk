8750213c89443ec98252c9adcde729ad5cacf715
tiigerbones <101901291+tiigerbones@users.noreply.github.com>
2025-08-05 18:05:13 -0600

    made changes to murk_whisper and added murk_vanish

--- src/main/java/com/enchantedwisp/murk/registry/Sounds.java
+++ src/main/java/com/enchantedwisp/murk/registry/Sounds.java
@@ -7,12 +7,18 @@ import net.minecraft.util.Identifier;
 
 public class Sounds {
     public static final SoundEvent MURK_WHISPERS = SoundEvent.of(Identifier.of("murk", "murk_whispers"));
+    public static final SoundEvent MURK_VANISH = SoundEvent.of(Identifier.of("murk", "murk_vanish"));
 
     public static void register() {
         Registry.register(
                 Registries.SOUND_EVENT,
                 Identifier.of("murk", "murk_whispers"),
                 MURK_WHISPERS
+        );
+        Registry.register(
+                Registries.SOUND_EVENT,
+                Identifier.of("murk", "murk_vanish"),
+                MURK_VANISH
         );
     }
 }
